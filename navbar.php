8a1bbb6d0d7ba4106babe3677cd89e88c353afa6
Sabri-oes <32087095+gy-sabri@users.noreply.github.com>
2018-02-17 18:21:12 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -44,7 +44,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="office_list.php">office_list</a>
